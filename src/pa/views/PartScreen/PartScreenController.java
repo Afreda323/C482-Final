49073219db945adb8549ef6858986c67510848b3
Anthony Freda <anthonyfreda323@gmail.com>
2019-02-18 14:08:40 -0500

    Add part logic

--- src/pa/views/PartScreen/PartScreenController.java
+++ src/pa/views/PartScreen/PartScreenController.java
@@ -15,7 +15,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
+import pa.models.InhousePart;
+import pa.models.Inventory;
+import pa.models.OutsourcedPart;
 
 /**
  * FXML Controller class
@@ -23,7 +30,31 @@ import javafx.stage.Stage;
  * @author anthonyfreda
  */
 public class PartScreenController implements Initializable {
-
+    @FXML
+    private final ToggleGroup radios = new ToggleGroup();
+    @FXML
+    private RadioButton InHouseRadio;
+    @FXML
+    private RadioButton OutsourcedRadio;
+    @FXML
+    private TextField IDInput;
+    @FXML
+    private TextField NameInput;
+    @FXML
+    private TextField InventoryInput;
+    @FXML
+    private TextField PriceInput;
+    @FXML
+    private TextField MaxInput;
+    @FXML
+    private TextField MinInput;
+    @FXML
+    private TextField COrMInput;
+    @FXML
+    private Label COrMText;
+    
+    private boolean inHouse = false;
+    
     /**
      * Called on cancel button click
      * @param event
@@ -37,13 +68,77 @@ public class PartScreenController implements Initializable {
         window.setScene(scene);
         window.show();
     }
+    
+    /**
+     * In house radio handler
+     * @param event 
+     */
+    @FXML
+    void handleInHouse(ActionEvent event) {
+        inHouse = true;
+        COrMText.setText("Machine ID");
+    }
+    /**
+     * Out source radio handler
+     * @param event 
+     */
+    @FXML
+    void handleOutsourced(ActionEvent event) {
+        inHouse = false;
+        COrMText.setText("Company Name");
+    }
 
+    /**
+     * Save or update a part
+     * @param event 
+     */
+    @FXML
+    void handleSave(ActionEvent event) throws IOException {
+        String name = NameInput.getText();
+        String inv = InventoryInput.getText();
+        String price = PriceInput.getText();
+        String min = MinInput.getText();
+        String max = MaxInput.getText();
+        String cOrM = COrMInput.getText();
+        if (inHouse) {
+            System.out.println("INHOUSE");
+            InhousePart part = new InhousePart();
+            part.setPartID(1);
+            part.setName(name);
+            part.setInStock(Integer.parseInt(inv));
+            part.setPrice(Double.parseDouble(price));
+            part.setMin(Integer.parseInt(min));
+            part.setMax(Integer.parseInt(max));
+            part.setMachineID(Integer.parseInt(cOrM));
+            Inventory.addPart(part);
+        } else {
+            OutsourcedPart part = new OutsourcedPart();
+            part.setPartID(2);
+            part.setName(name);
+            part.setInStock(Integer.parseInt(inv));
+            part.setPrice(Double.parseDouble(price));
+            part.setMin(Integer.parseInt(min));
+            part.setMax(Integer.parseInt(max));
+            part.setCompanyName(cOrM);
+            Inventory.addPart(part);
+        }
+        Parent loader = FXMLLoader.load(getClass().getResource("/pa/views/MainScreen/MainScreen.fxml"));
+        Scene scene = new Scene(loader);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        InHouseRadio.setToggleGroup(radios);
+        OutsourcedRadio.setToggleGroup(radios);
+        inHouse = true;
+        COrMText.setText("Machine ID");
+        IDInput.setPromptText("Auto-Generated");
     }
 
 }
